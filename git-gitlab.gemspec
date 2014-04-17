7f305747b38d17984a7a4882475f5ae3aeccb7bc
numa08 <n511287@gmail.com>
2014-04-18 01:18:09 +0900

    Authorize gitlab user account
    when input `git gitlab`, puts some imformations.
    show
     - username
     - gitlab api endpoint url
    fixes #1

--- git-gitlab.gemspec
+++ git-gitlab.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = Git::Gitlab::VERSION
   spec.authors       = ["numa08"]
   spec.email         = ["n511287@gmail.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
+  spec.description   = "Gitlab Command line interface"
+  spec.summary       = "git-gitlab command line"
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "thor" , "~> 0.19.1"
+  spec.add_development_dependency "gitlab", "~> 3.0.0"
+  spec.add_development_dependency "grit", "~> 2.5.0"
 end
--- lib/git/gitlab.rb
+++ lib/git/gitlab.rb
@@ -1,7 +1,42 @@
 require "git/gitlab/version"
+require "gitlab"
+require "grit"
 
-module Git
-  module Gitlab
-    # Your code goes here...
-  end
+module GitlabApi
+	API_VERSION = "v3"
+	USER_AGENT = "nyonyonyonyonyo! Gitlab nyo!!"
+	attr_reader :client
+
+	def initialize
+		config = Grit::Repo.new(`git rev-parse --show-toplevel`.chomp).config
+
+		url = config["gitlab.url"]
+		if url == nil
+			raise "Plase set 'git config gitlab.url ${Gitlab URL}'"
+		end
+
+		token = config["gitlab.token"]
+		if token == nil
+			raise "Please set 'git config gitlab.token ${Gitlab Token}'"
+		end
+
+		Gitlab.configure do |config|
+		  config.endpoint       = "#{url}/api/#{API_VERSION}"
+		  config.private_token  = token
+		  config.user_agent     = USER_AGENT
+		end
+
+		@client = Gitlab.client
+	end
+
+	def authorize
+		user = @client.user
+		puts("You are #{user.username}")		
+		puts("Gitlab is #{Gitlab.endpoint}")
+	end
+end
+
+
+class GitlabKernel
+	include GitlabApi
 end
