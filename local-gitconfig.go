a24eb030e5ebdcec7dd50cfa54d370d04c436913
numa08 <n511287@gmail.com>
2016-05-21 10:53:09 +0900

    git2go.v24

--- local-gitconfig.go
+++ local-gitconfig.go
@@ -1,35 +1,36 @@
 package main
+
 import (
-    "gopkg.in/libgit2/git2go.v23"
-    "os/exec"
-    "strings"
-    "fmt"
+	"fmt"
+	"gopkg.in/libgit2/git2go.v24"
+	"os/exec"
+	"strings"
 )
 
 type LocalGitConfig struct {
-    Config *git.Config
+	Config *git.Config
 }
 
 func NewLocalGitConfig() (*LocalGitConfig, error) {
-    path, e := exec.Command("git", "rev-parse", "--show-toplevel").Output()
-    p := strings.TrimRight(string(path), "\n")
-    repo, e := git.OpenRepository(p)
-    conf, e := repo.Config()
-    config := LocalGitConfig{
-        Config: conf,
-    }
-    return &config, e
+	path, e := exec.Command("git", "rev-parse", "--show-toplevel").Output()
+	p := strings.TrimRight(string(path), "\n")
+	repo, e := git.OpenRepository(p)
+	conf, e := repo.Config()
+	config := LocalGitConfig{
+		Config: conf,
+	}
+	return &config, e
 }
 
 func (this *LocalGitConfig) Host() (string, error) {
-    return this.Config.LookupString("gitlab.url")
+	return this.Config.LookupString("gitlab.url")
 }
 
 func (this *LocalGitConfig) Token() (string, error) {
-    return this.Config.LookupString("gitlab.token")
+	return this.Config.LookupString("gitlab.token")
 }
 
 func (this *LocalGitConfig) ApiPath() (string, error) {
-    apiPath := fmt.Sprintf("/api/v3")
-    return apiPath, nil
+	apiPath := fmt.Sprintf("/api/v3")
+	return apiPath, nil
 }
